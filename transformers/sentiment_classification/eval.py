f5f68d7aa0f8f6e8464a795406ebb5033ab3879a
Varun Suresh <fab.varun@gmail.com>
2024-11-18 15:33:29 -0800

    Similar to train script, make it easy to run from a notebook

--- transformers/sentiment_classification/eval.py
+++ transformers/sentiment_classification/eval.py
@@ -2,63 +2,50 @@
 Evaluate fine-tuned GPT-2 on IMDb movie reviews
 """
 
-import click
 from tqdm import tqdm
 import torch
 from torch.utils.data.dataloader import DataLoader
-from reviewsDataset import reviewsDataset
+from sentiment_classification.reviewsDataset import reviewsDataset
 from gpt_utils import dynamic_padding
 from gpt_config import GPTConfig
+from sentiment_classification.eval_config import EvalConfig
 from gpt import GPT
 
-device = "mps"
-# Zero Shot Eval
-config = GPTConfig(use_lora=False)
-model = GPT.from_pretrained()
-model.crop_block_size(config.block_size)
-
-# config = GPTConfig(binary_classification_head=True,block_size=128)
-# if config.use_lora:
-#     checkpoint = torch.load("out/ckpt_lora.pt")
-#     model = GPT.from_pretrained(config=GPTConfig(binary_classification_head=True))
-#     model.crop_block_size(128)
-#     model.load_state_dict(checkpoint["model"],strict=False)
-# else:
-#     checkpoint = torch.load("out/ckpt.pt")
-#     model = GPT(config)
-#     model.load_state_dict(checkpoint["model"])
-
-model.to(device=device)
-model.eval()
-
-@click.command()
-@click.option("--split",default="test",type=click.Choice(["test","train"]))
-@click.option("--batch_size",default=2)
-@click.option("--max_length",default=128)
-@click.option("--results_fname",default="zero_shot.txt")
-@click.option("--subset",default=True)
-def run_inference(split,batch_size,max_length,results_fname,subset):
-    rd = reviewsDataset(split=split,max_length=max_length)
-    if subset:
-        subset_range = torch.arange(0,len(rd),10)
-        dl = DataLoader(torch.utils.data.Subset(rd,subset_range),batch_size=batch_size,collate_fn=dynamic_padding)
-    else:
-        dl = DataLoader(rd,batch_size=batch_size,collate_fn=dynamic_padding)
+class Eval:
+    def __init__(self,test_set: reviewsDataset,eval_config: EvalConfig, model_config: GPTConfig):
+        self.test_set = test_set
+        self.eval_config = eval_config
+        self.model_config = model_config
+        self.load_model()
     
-    results_file = open(results_fname,"w")
-    results_file.write(f"filename,length,label,prediction,pos_probability,neg_probability\n")
-    for batch in tqdm(dl):
-        with torch.no_grad():
-            logits, _ = model(batch["input_ids"].to(device),batch["attention_masks"].to(device))
-            sentiment_idx = rd.get_pos_neg_indices()
-            for i, fname in enumerate(batch["fpaths"]):
-                pos = logits[i,sentiment_idx["positive"]]
-                neg = logits[i,sentiment_idx["negative"]]
-                if pos > neg:
-                    prediction = 1
-                else:
-                    prediction = 0
- 
-                results_file.write(f"{fname},{batch['lengths'][i]},{batch['labels'][i]},{prediction},{pos},{neg}\n")
-if __name__ == "__main__":
-    run_inference()
+    def load_model(self):
+        self.model = GPT.from_pretrained(config=self.model_config)
+        if self.model_config.load_from_checkpoint:
+            ckpt = torch.load(self.model_config.checkpoint_path,map_location=self.eval_config.device)
+            self.model.load_state_dict(ckpt["model"])
+        self.model.to(self.eval_config.device)
+        self.model.eval()
+
+    def evaluate(self):
+        if self.eval_config.subset:
+            subset_range = torch.arange(0,len(self.test_set),self.eval_config.interval)
+            dl = DataLoader(torch.utils.data.Subset(self.test_set,subset_range),batch_size=self.eval_config.batch_size,collate_fn=dynamic_padding)
+        else:
+            dl = DataLoader(self.test_set,batch_size=self.eval_config.batch_size,collate_fn=dynamic_padding)
+
+        results_file = open(self.eval_config.results_path,"w")
+        results_file.write("filename,length,label,prediction,logit_pos,logit_neg\n")
+        for batch in tqdm(dl):
+            with torch.no_grad():
+                logits, _ = self.model(batch["input_ids"].to(self.eval_config.device),batch["attention_masks"].to(self.eval_config.device))
+                sentiment_idx = self.test_set.get_pos_neg_indices()
+                for i, fname in enumerate(batch["fpaths"]):
+                    pos = logits[i,sentiment_idx["positive"]]
+                    neg = logits[i,sentiment_idx["negative"]]
+                    if pos > neg:
+                        prediction = 1
+                    else:
+                        prediction = 0
+    
+                    results_file.write(f"{fname},{batch['lengths'][i]},{batch['labels'][i]},{prediction},{pos},{neg}\n")               
+
