32363c5da22fa4b22f2de9fc4f01a955ec7fe19e
Varun Suresh <fab.varun@gmail.com>
2024-11-18 15:32:00 -0800

    Similar to train config

---
+++ transformers/sentiment_classification/eval_config.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class EvalConfig:
+
+    # Device params
+    device: str = "mps"
+
+    # I/O
+    results_path: str = "results.txt"
+    
+    # Dataset 
+    batch_size: int = 2
+    subset: bool = True # Runs only on a subset of the split. To quickly evaluate whether the model is working as expected
+    interval: int = 100 # If subset is true, then pick one sample out of every interval(100 by default) samples.
+    
